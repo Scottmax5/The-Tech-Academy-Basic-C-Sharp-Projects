84f5fc5db73e089905d4cf9d9fef4bc201762dcd
Scottmax5 <127995861+Scottmax5@users.noreply.github.com>
2023-07-31 14:50:06 -0700

    Method Class

---
+++ Method-Class-Assignment/Method-Class-Assignment/MathMethod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignmentSubmission
+{
+    public class MathMethod
+    {
+        public void mathOp(int a, int b)
+        {
+            int result = a * 10;
+            Console.WriteLine($"Result: {result}, Second Number: {b}");
+        }
+    }
+}
---
+++ Method-Class-Assignment/Method-Class-Assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignmentSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathMethod mathOperation = new MathMethod();
+
+            // Call the method in the class, passing in two numbers
+            mathOperation.mathOp(5, 10);
+
+            // Call the method in the class, specifying the parameters by name
+            mathOperation.mathOp(a: 7, b: 20);
+
+            Console.ReadLine();
+        }
+    }
+}
