4e9125b2f4118b6463954b141430272207ea87fb
Scottmax5 <127995861+Scottmax5@users.noreply.github.com>
2023-08-01 16:57:11 -0700

    Operator assignment

---
+++ Operators Assignment/Operators Assignment/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Assignment
+{
+    public class Employee
+    {
+        //firstName and lastName properties
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //== Checks if both employees are equal
+        public static bool operator ==(Employee emp1, Employee emp2)
+        {
+            //If both objects are null, or both have the same reference, they are equal
+            if (ReferenceEquals(emp1, emp2))
+                return true;
+
+            //If one of the objects is null, they are not equal
+            if (emp1 is null || emp2 is null)
+                return false;
+
+            //Compare the Id properties to determine equality
+            return emp1.Id == emp2.Id;
+        }
+
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            return !(emp1 == emp2);
+        }
+    }
+}
---
+++ Operators Assignment/Operators Assignment/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Assignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Instantiate two Employee objects
+            Employee emp1 = new Employee();
+            Employee emp2 = new Employee();
+
+            //Assign values to the properties of the first Employee
+            emp1.Id = 1;
+            emp1.FirstName = "John";
+            emp1.LastName = "Doe";
+
+            //Assign values to the properties of the second Employee
+            emp2.Id = 2;
+            emp2.FirstName = "Jane";
+            emp2.LastName = "Smith";
+
+            //Compare the two Employees using the overloaded "==" operator
+            if (emp1 == emp2)
+            {
+                Console.WriteLine("The Employee objects are equal.");
+            }
+            else
+            {
+                Console.WriteLine("The Employee objects are not equal.");
+            }
+
+            //Compare the two Employees using the overloaded "!=" operator
+            if (emp1 != emp2)
+            {
+                Console.WriteLine("The Employee objects are not equal.");
+            }
+            else
+            {
+                Console.WriteLine("The Employee objects are equal.");
+            }
+        }
+    }
+}
